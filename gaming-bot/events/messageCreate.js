62afc199a2eb3c77db17bfd23627808ff192d500
hghgh57 <rizzbut3@gmail.com>
2026-07-25 23:56:27 +1000

    Update messageCreate.js

--- gaming-bot/events/messageCreate.js
+++ gaming-bot/events/messageCreate.js
@@ -1,19 +1,27 @@
 const { getSticky, updateLastMessageId } = require('../utils/stickyManager');
+const { addXpForMessage } = require('../utils/levelManager');
 
 module.exports = {
   name: 'messageCreate',
   async execute(message) {
     if (message.author.bot) return;
 
-    const sticky = getSticky(message.channel.id);
-    if (!sticky) return;
-
-    if (sticky.lastMessageId) {
-      const oldMsg = await message.channel.messages.fetch(sticky.lastMessageId).catch(() => null);
-      if (oldMsg) await oldMsg.delete().catch(() => {});
+    const xpResult = addXpForMessage(message.author.id);
+    if (xpResult && xpResult.leveledUp) {
+      await message.channel
+        .send(`🎉 ${message.author} leveled up to **level ${xpResult.newLevel}**!`)
+        .catch(() => {});
     }
 
-    const sent = await message.channel.send(`📌 ${sticky.content}`).catch(() => null);
-    if (sent) updateLastMessageId(message.channel.id, sent.id);
+    const sticky = getSticky(message.channel.id);
+    if (sticky) {
+      if (sticky.lastMessageId) {
+        const oldMsg = await message.channel.messages.fetch(sticky.lastMessageId).catch(() => null);
+        if (oldMsg) await oldMsg.delete().catch(() => {});
+      }
+
+      const sent = await message.channel.send(`📌 ${sticky.content}`).catch(() => null);
+      if (sent) updateLastMessageId(message.channel.id, sent.id);
+    }
   },
 };
