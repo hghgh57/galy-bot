ab80d09b774f56fb7a22abefe7ae9b6859237a39
hghgh57 <rizzbut3@gmail.com>
2026-07-26 20:26:53 +1000

    Change bot activity message on ready event

--- gaming-bot/events/ready.js
+++ gaming-bot/events/ready.js
@@ -5,7 +5,7 @@ module.exports = {
   once: true,
   execute(client) {
     console.log(`✅ Logged in as ${client.user.tag}`);
-    client.user.setActivity('for /ticket-panel', { type: 3 });
+    client.user.setActivity('voidddddddd', { type: 2 });
     startTikTokPolling(client);
   },
 };
