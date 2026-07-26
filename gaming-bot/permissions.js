09d64162d05b035ce65985f5213d23000874c7c8
hghgh57 <rizzbut3@gmail.com>
2026-07-26 14:42:04 +1000

    Delete gaming-bot/permissions.js

--- gaming-bot/permissions.js
+++
@@ -1,8 +0,0 @@
-const config = require('../config.json');
-
-function isAdmin(member) {
-  const roleIds = config.adminRoleIds || [];
-  return roleIds.some((id) => id && !id.startsWith('PUT_') && member.roles.cache.has(id));
-}
-
-module.exports = { isAdmin };
