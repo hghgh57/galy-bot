49b8d2c8de061d5618d5f38845a534ab2ff66c86
hghgh57 <rizzbut3@gmail.com>
2026-07-26 20:29:57 +1000

    Update ready.js

--- gaming-bot/events/ready.js
+++ gaming-bot/events/ready.js
@@ -5,7 +5,19 @@ module.exports = {
   once: true,
   execute(client) {
     console.log(`✅ Logged in as ${client.user.tag}`);
-    client.user.setActivity('voidddddddd', { type: 2 });
     startTikTokPolling(client);
+
+    const statuses = [
+      { name: 'voiddd', type: 2 },
+      { name: 'stalking voidz toilet', type: 3 },
+    ];
+
+    let index = 0;
+    client.user.setActivity(statuses[index].name, { type: statuses[index].type });
+
+    setInterval(() => {
+      index = (index + 1) % statuses.length;
+      client.user.setActivity(statuses[index].name, { type: statuses[index].type });
+    }, 10 * 1000);
   },
 };
