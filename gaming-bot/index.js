a77c838ebb36c15ce13b8c74c13ecbbdfc42c523
hghgh57 <rizzbut3@gmail.com>
2026-08-10 17:47:10 +1000

    Update index.js

--- gaming-bot/index.js
+++ gaming-bot/index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const fs = require('fs');
 const path = require('path');
 const { Client, GatewayIntentBits, Collection, Partials } = require('discord.js');
+
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
