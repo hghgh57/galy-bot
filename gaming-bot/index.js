60fdad9deff33f799139d5339f194938fc56219c
hghgh57 <rizzbut3@gmail.com>
2026-08-20 11:53:08 +1000

    Update index.js

--- gaming-bot/index.js
+++ gaming-bot/index.js
@@ -10,6 +10,7 @@ const client = new Client({
     GatewayIntentBits.GuildMembers,
     GatewayIntentBits.MessageContent,
     GatewayIntentBits.DirectMessages,
+    GatewayIntentBits.GuildVoiceStates,
   ],
   partials: [Partials.Channel],
 });
